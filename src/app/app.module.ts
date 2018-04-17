b02b885f32ce0bd2a3406a4af9a64de7ed9866e8
Juanca <juancasan17@gmail.com>
2018-04-17 16:31:24 -0500

    add rest of ui pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { BrandsComponent } from './brands/brands.component';
+import { ContactComponent } from './contact/contact.component';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'catalog', component: CatalogComponent},
+  { path: 'brands', component: BrandsComponent},
+  { path: 'contact', component: ContactComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogComponent,
+    AboutComponent,
+    BrandsComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
